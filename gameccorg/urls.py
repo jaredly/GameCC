e69388d13e7e4b820a50c5904932124607d01359
Jared Forsyth <jared@jaredforsyth.com>
2010-05-03 10:08:12 -0400

    theme changes, adding some website tabs

--- gameccorg/urls.py
+++ gameccorg/urls.py
@@ -11,18 +11,15 @@ import views
 urlpatterns = patterns('',
     (r'^$', views.home),
     (r'^browse/(?:(?P<category>.+)/)?$', views.browse),
-    (r'^categories/$', views.categories),
+    (r'^create/$', views.create),
+
+    (r'^editor/', include('gcc_editor.urls')),
 
     (r'^blog/', include('basic.blog.urls')),
-    # Example:
-    # (r'^gameccorg/', include('gameccorg.foo.urls')),
 
     (r'^media/(?P<path>.*)$', serve_media.serve_all, {'document_root' : settings.MEDIA_ROOT}),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
+    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
 )
--- gameccorg/views.py
+++ gameccorg/views.py
@@ -4,33 +4,62 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from myprojects.models import Project
 from basic.blog.models import Post
+from django.views.generic import date_based, list_detail
+## my login_required
+from decorators import my_login_required
 
 from django.http import HttpResponseRedirect
 
 from django.contrib import messages
 
-def home(request):
-    return render_to_response('home.html',{'projects':Project.objects.all()[:8],
-        'posts':Post.objects.all()[:3]}, context_instance = RequestContext(request))
+from gcc_projects.models import Project, Category
 
-def browse(request, slug=None):
+def home(request):
+    return render_to_response('home.html',{
+            'projects':Project.objects.all()[:8],
+            'posts':Post.objects.all()[:3]
+        }, context_instance = RequestContext(request))
+
+def create(request):
+    if not request.user.is_authenticated():
+        return render_to_response('login.html', {'to_url':'/create/'},
+                context_instance=RequestContext(request))
+    return render_to_response('create.html', {
+            'my_projects':request.user.my_projects,
+        }, context_instance=RequestContext(request))
+create = my_login_required(create, message='''You must be logged in to create and edit projects. Please login below or `register an account </registration/new_user/>`_''')
+
+def browse(request, category=None):
+    if not category: category = 'latest'
     if category == 'latest':
-        return browse_latest()
-    elif category == 'popular':
-        return browse_popular()
-    try:
-        cat = Category.objects.get(slug__iexact=slug)
         return list_detail.object_list(
                 request,
-                queryset=cat.project_set.all(),
-                template_name='category.html'
+                queryset=Project.objects.all().order_by('-modified'),
+                template_name='gamecc/project_list.html',
             )
-    except Category.DoesNotExist:
-        messages.set_level(request, messges.ERROR)
-        messages.error(request, 'That category doesn\'t exist')
-    return HttpResponseRedirect('/categories/')
-
-def categories(request):
-    return fail ### WORK HERE###
+    elif category == 'popular':
+        return list_detail.object_list(
+                request,
+                queryset=Project.objects.all().order_by('-modified'),
+                template_name='gamecc/project_list.html',
+            )
+    elif category == 'categories':
+        return list_detail.object_list(
+                request,
+                queryset=Category.objects.all(),
+                template_name='gamecc/categories.html',
+            )
+    else:
+        try:
+            cat = Category.objects.get(slug__iexact=category)
+            return list_detail.object_list(
+                    request,
+                    queryset=cat.project_set,
+                    template_name='gamecc/category.html'
+                )
+        except Category.DoesNotExist:
+            messages.set_level(request, messges.ERROR)
+            messages.error(request, 'That category doesn\'t exist')
+        return HttpResponseRedirect('/browse/categories/')
 
 # vim: et sw=4 sts=4
